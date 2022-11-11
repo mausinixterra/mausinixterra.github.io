3e1866276e675543f32b4f5d5df2abe719a0f70b
Diego Mauricio Viveros <diegocali114@hotmail.com>
2022-11-11 14:54:53 -0500

    Feat: Seccion de sobre mi

--- assets/js/main.js
+++ assets/js/main.js
@@ -245,3 +245,19 @@
   });
 
 })()
+
+function calcularEdad(fecha) {
+    var hoy = new Date();
+    var cumpleanos = new Date(fecha);
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+
+    return edad;
+}
+
+document.getElementById("edad").innerHTML = calcularEdad("1993-12-22");
+
